7becbbfe2b1b2fff7f71b680be5473e6a1c3051e
baz1 <baz1.github@gmail.com>
2015-10-29 15:33:44 +0000

    Split string operation

---
+++ src/strop.cpp
@@ -0,0 +1,56 @@
+#include "strop.h"
+
+/*!
+    \headerfile <algo/strop>
+    \inmodule Algo
+    \ingroup Algo
+
+    \title String operations
+    \brief This file provides string operations to complete the <string> library.
+*/
+
+/*!
+    \relates <algo/strop>
+    Splits a string.
+
+    Splits the string \a haystack with the separator \a needle, and returns the result as a string
+    vector which members may be empty. If \a min_capacity is non-zero,
+    it will be used as the initial capacity for the resulting vector.
+*/
+std::vector<std::string> split(const std::string &haystack, const char needle, int min_capacity)
+{
+    std::vector<std::string> result;
+    if (min_capacity)
+        result.reserve(min_capacity);
+    size_t i = 0, j;
+    while ((j = haystack.find(needle, i)) != std::string::npos)
+    {
+        result.push_back(haystack.substr(i, j - i));
+        i = j + 1;
+    }
+    result.push_back(haystack.substr(i));
+    return result;
+}
+
+/*!
+    \relates <algo/strop>
+    Splits a string.
+
+    Splits the string \a haystack with the separator \a needle, and returns the result as a string
+    vector which members may be empty. If \a min_capacity is non-zero,
+    it will be used as the initial capacity for the resulting vector.
+*/
+std::vector<std::string> split(const std::string &haystack, const std::string &needle, int min_capacity)
+{
+    std::vector<std::string> result;
+    if (min_capacity)
+        result.reserve(min_capacity);
+    size_t i = 0, j;
+    while ((j = haystack.find(needle, i)) != std::string::npos)
+    {
+        result.push_back(haystack.substr(i, j - i));
+        i = j + needle.length();
+    }
+    result.push_back(haystack.substr(i));
+    return result;
+}
---
+++ src/strop.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2015, Rémi Bazin <bazin.remi@gmail.com>
+ * All rights reserved.
+ * See LICENSE for licensing details.
+ */
+
+#ifndef ALGRB_STROP_H
+#define ALGRB_STROP_H
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> split(const std::string &haystack, const char needle, int min_capacity = 0);
+std::vector<std::string> split(const std::string &haystack, const std::string &needle, int min_capacity = 0);
+
+#endif // ALGRB_STROP_H
