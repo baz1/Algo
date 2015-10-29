fab92f0420da344fbe54ba8232246a674c8d306e
baz1 <baz1.github@gmail.com>
2015-10-29 23:36:04 +0000

    Positive assumption in gcd functions removed

--- algo/numop.h
+++ algo/numop.h
@@ -30,16 +30,11 @@ template <typename T> inline const T &qMax(const T &a, const T &b) { return (a <
 
 template <typename T> T gcd(T a, T b)
 {
-    ASSERT((a >= 0) && (b >= 0));
-    T c;
-    if (b < a)
-        goto invcase;
     while (true)
     {
         if (a == 0)
             return b;
         b %= a;
-invcase:
         if (b == 0)
             return a;
         a %= b;
@@ -48,32 +43,23 @@ invcase:
 
 template <typename T> std::tuple<T,T,T> gcdex(T v1, T v2)
 {
-    ASSERT((v1 >= 0) && (v2 >= 0));
     T a1 = 1, b1 = 0, a2 = 0, b2 = 1, q;
-    if (v1 < v2)
-    {
-        if (v1 == 0)
-            return std::tuple<T,T,T>(a2, b2, v2);
-        goto invcase;
-    } else {
-        if (v2 == 0)
-            return std::tuple<T,T,T>(a1, b1, v1);
-        goto normalcase;
-    }
+    if (v2 == 0)
+        return std::tuple<T,T,T>(a1, b1, v1);
+    goto midloop;
     while (true)
     {
         if (v2 == 0)
             return std::tuple<T,T,T>(a1, b1, v1);
         a2 -= q * a1;
         b2 -= q * b1;
-normalcase:
+midloop:
         q = v1 / v2;
         v1 -= q * v2;
         if (v1 == 0)
             return std::tuple<T,T,T>(a2, b2, v2);
         a1 -= q * a2;
         b1 -= q * b2;
-invcase:
         q = v2 / v1;
         v2 -= q * v1;
     }
@@ -81,10 +67,7 @@ invcase:
 
 template <typename T> std::tuple<T,T,T,T,T> gcdex2(T v1, T v2)
 {
-    ASSERT((v1 >= 0) && (v2 >= 0));
     T a1 = 1, b1 = 0, a2 = 0, b2 = 1, q;
-    if (v1 < v2)
-        goto invcase;
     while (true)
     {
         if (v2 == 0)
@@ -93,7 +76,6 @@ template <typename T> std::tuple<T,T,T,T,T> gcdex2(T v1, T v2)
         v1 -= q * v2;
         a1 -= q * a2;
         b1 -= q * b2;
-invcase:
         if (v1 == 0)
             return std::tuple<T,T,T,T,T>(a2, b2, v2, qAbs(b1), qAbs(a1));
         q = v2 / v1;
